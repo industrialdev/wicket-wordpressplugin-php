8db2acc68dd5f71f5778659f89a721b6d9922a9f
Kevin Andrews <kandrews@industrialagency.ca>
2019-05-15 15:07:05 -0400

    matched form markup to sassquatch standard

--- wicket_update_password.php
+++ wicket_update_password.php
@@ -140,51 +140,56 @@ class wicket_update_password extends WP_Widget {
 		<?php endif; ?>
 
 		<form class='manage_password_form' method="post">
-			<label class="form__label" for="current_password"><?php _e('Current password') ?>
-				<span class="required">*</span>
-				<?php
-				if (isset($_SESSION['wicket_password_form_errors']) && !empty($_SESSION['wicket_password_form_errors'])) {
-					foreach ($_SESSION['wicket_password_form_errors'] as $key => $error) {
-						if (isset($error->meta->field) && $error->meta->field == 'user.current_password') {
-							echo "<span class='error'>".__('Current password')." {$error->title}</span>";
-							$current_password_err = true;
+			<div class="form__group">
+				<label class="form__label" for="current_password"><?php _e('Current password') ?>
+					<span class="required">*</span>
+					<?php
+					if (isset($_SESSION['wicket_password_form_errors']) && !empty($_SESSION['wicket_password_form_errors'])) {
+						foreach ($_SESSION['wicket_password_form_errors'] as $key => $error) {
+							if (isset($error->meta->field) && $error->meta->field == 'user.current_password') {
+								echo "<span class='error'>".__('Current password')." {$error->title}</span>";
+								$current_password_err = true;
+							}
 						}
 					}
-				}
-				?>
-			</label>
-			<input class="form__input" <?php if (isset($current_password_err) && $current_password_err): echo "class='error_input'"; endif; ?> required type="password" id="current_password" name="current_password" value="">
-
-			<label class="form__label" for="password"><?php _e('New password') ?>
-				<span class="required">*</span>
-				<?php
-				if (isset($_SESSION['wicket_password_form_errors']) && !empty($_SESSION['wicket_password_form_errors'])) {
-					foreach ($_SESSION['wicket_password_form_errors'] as $key => $error) {
-						if (isset($error->meta->field) && $error->meta->field == 'user.password') {
-							echo "<span class='error'>".__('New password')." {$error->title}</span>";
-							$password_err = true;
+					?>
+				</label>
+				<input class="form__input" <?php if (isset($current_password_err) && $current_password_err): echo "class='error_input'"; endif; ?> required type="password" id="current_password" name="current_password" value="">
+			</div>
+			
+			<div class="form__group">
+				<label class="form__label" for="password"><?php _e('New password') ?>
+					<span class="required">*</span>
+					<?php
+					if (isset($_SESSION['wicket_password_form_errors']) && !empty($_SESSION['wicket_password_form_errors'])) {
+						foreach ($_SESSION['wicket_password_form_errors'] as $key => $error) {
+							if (isset($error->meta->field) && $error->meta->field == 'user.password') {
+								echo "<span class='error'>".__('New password')." {$error->title}</span>";
+								$password_err = true;
+							}
 						}
 					}
-				}
-				?>
-			</label>
-
-			<input class="form__input" <?php if (isset($password_err) && $password_err): echo "class='error_input'"; endif; ?> required type="password" name="password" id="password" value="">
-
-			<label class="form__label" for="password_confirmation"><?php _e('Confirm new password') ?>
-				<span class="required">*</span>
-				<?php
-				if (isset($_SESSION['wicket_password_form_errors']) && !empty($_SESSION['wicket_password_form_errors'])) {
-					foreach ($_SESSION['wicket_password_form_errors'] as $key => $error) {
-						if (isset($error->meta->field) && $error->meta->field == 'user.password_confirmation') {
-							echo "<span class='error'>".__('Confirm password')." {$error->title}</span>";
-							$password_confirm_err = true;
+					?>
+				</label>
+				<input class="form__input" <?php if (isset($password_err) && $password_err): echo "class='error_input'"; endif; ?> required type="password" name="password" id="password" value="">
+			</div>
+			
+			<div class="form__group">
+				<label class="form__label" for="password_confirmation"><?php _e('Confirm new password') ?>
+					<span class="required">*</span>
+					<?php
+					if (isset($_SESSION['wicket_password_form_errors']) && !empty($_SESSION['wicket_password_form_errors'])) {
+						foreach ($_SESSION['wicket_password_form_errors'] as $key => $error) {
+							if (isset($error->meta->field) && $error->meta->field == 'user.password_confirmation') {
+								echo "<span class='error'>".__('Confirm password')." {$error->title}</span>";
+								$password_confirm_err = true;
+							}
 						}
 					}
-				}
-				?>
-			</label>
-			<input class="form__input" <?php if (isset($password_confirm_err) && $password_confirm_err): echo "class='error_input'"; endif; ?> type="password" id="password_confirmation" name="password_confirmation" value="">
+					?>
+				</label>
+				<input class="form__input" <?php if (isset($password_confirm_err) && $password_confirm_err): echo "class='error_input'"; endif; ?> type="password" id="password_confirmation" name="password_confirmation" value="">
+			</div>
 
 			<input class="button button--primary" type="submit" value="<?php _e('Change password') ?>">
 		</form>
