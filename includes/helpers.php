e26a935b28d2bb4e8f09046cb5f24e62e7e46a7d
Rob Ferguson <rferguson@industrialagency.ca>
2021-09-03 09:03:13 -0400

    fixed wicket_add_data_field in readonly mode to detect empty arrays properly

--- includes/helpers.php
+++ includes/helpers.php
@@ -432,7 +432,7 @@ function wicket_add_data_field(&$data_fields, $field, $schema, $type, $entity = 
     if ($type == 'readonly') {
       // make sure, usually on new accounts, that there is even AI fields to read from
       // data_fields will likely be completely empty on new accounts
-      if (!empty($entity->data_fields)) {
+      if (!empty((array)$entity->data_fields)) {
         foreach ($entity->data_fields as $df) {
           if ($df['$schema'] == $schema) {
             // look for existing value, if there is one, else ignore this field
